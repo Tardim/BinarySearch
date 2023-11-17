cef98c9cd1ab7cdf14168d9f691516837e338e69
mstar <tardim@go.stockton.edu>
2023-11-17 10:05:07 -0500

    Initial commit

---
+++ src/YNsearch.java
@@ -0,0 +1,15 @@
+public class YNsearch {
+
+    int rightValue;
+    int leftValue;
+    public int addValue(){
+
+    }
+    public void remove(){
+
+    }
+    public int findLargest(){
+
+    }
+
+}
